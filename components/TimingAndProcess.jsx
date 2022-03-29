5b3979f541d8a39a9e01c9629c984362b680fc54
BaranKandil <barankandil10@gmail.com>
2022-03-29 14:59:54 +0300

    Add divider hr between 1,2,3 cols

--- components/TimingAndProcess.jsx
+++ components/TimingAndProcess.jsx
@@ -3,43 +3,38 @@ import React from 'react'
 const TimingAndProcess = () => {
   return (
     <div className="lg:mx-44 lg:mt-20 mx-16 mt-8">
-      <div className="flex justify-between relative">
-        <div className="shrink-0 max-w-[14.375rem]">
-          <div className="bg-ycc-green w-12 h-12 border-2 border-ycc-green flex items-center justify-center shrink-0">
-            <span className="text-white text-4xl text-bold nums">1</span>
+      <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-1 lg:grid-cols-3 gap-10 justify-between relative">
+          <div className="flex flex-row items-start lg:flex-col">
+            <div className="flex flex-col lg:flex-row items-center lg:justify-center shrink-0 mr-10 lg:mr-0 lg:w-full">
+              <span className="text-white w-12 h-12 bg-ycc-green text-4xl text-bold nums flex items-center justify-center shrink-0 ">1</span>
+              <span className="w-px h-20 mt-10 lg:mt-0 lg:h-px lg:w-full bg-ycc-green lg:ml-10"></span>
+            </div>
+            <div className="page__content page__content--timing-and-process">
+                <h4 className="mt-0 lg:mt-5">APPLY</h4>
+                <p>Send us your GitHub link and LinkedIn profile via Apply Now section.</p>
+            </div>
           </div>
-          <div className="page__content">
-              <h4>APPLY</h4>
-              <p>Send us your GitHub link and LinkedIn profile via Apply Now section.</p>
+          <div className="flex flex-row items-start lg:flex-col">
+            <div className="flex flex-col lg:flex-row items-center lg:justify-center shrink-0 mr-10 lg:mr-0 lg:w-full">
+              <span className="text-white w-12 h-12 bg-ycc-green text-4xl text-bold nums flex items-center justify-center shrink-0 ">2</span>
+              <span className="w-px h-20 mt-10 lg:mt-0 lg:h-px lg:w-full bg-ycc-green lg:ml-10"></span>
+            </div>
+            <div className="page__content page__content--timing-and-process">
+                <h4 className="mt-0 lg:mt-5">TEST</h4>
+                <p>Send us your GitHub link and LinkedIn profile via Apply Now section.</p>
+            </div>
           </div>
-        </div>
-        {/* <div className="border border-ycc-green h-0 w-[30%] left-16 mt-6 absolute" /> */}
-        <div className="shrink-0 max-w-[14.375rem]">
-          <div className="bg-ycc-green w-12 h-12 border-2 border-ycc-green flex items-center justify-center shrink-0">
-            <span className="text-white text-4xl text-bold nums">2</span>
+          <div className="flex flex-row items-start lg:flex-col">
+            <div className="flex flex-col lg:flex-row items-center lg:justify-center shrink-0 mr-10 lg:mr-0">
+              <span className="text-white w-12 h-12 bg-ycc-green text-4xl text-bold nums flex items-center justify-center shrink-0 ">3</span>
+            </div>
+            <div className="page__content page__content--timing-and-process">
+                <h4 className="mt-0 lg:mt-5">INTERVIEW</h4>
+                <p>Send us your GitHub link and LinkedIn profile via Apply Now section.</p>
+            </div>
           </div>
-          <div className="page__content">
-            <h4>TEST</h4>
-            <p>We will send you a technical test and ask you to complete it in the given time.</p>
-          </div>
-        </div>
-        {/* <div className="border border-ycc-green h-0 w-[30%] left-16 mt-6 absolute" /> */}
-        <div className="shrink-0  max-w-[14.375rem]">
-          <div className="bg-ycc-green w-12 h-12 border-2 border-ycc-green flex items-center justify-center shrink-0">
-            <span className="text-white text-4xl text-bold nums">3</span>
-          </div>
-          <div className="page__content">
-            <h4>INTERVIEW</h4>
-            <p>We will conduct a short interview with you.</p>
-        </div>
         </div>
       </div>
-      {/* <div className="flex gap-x-[9.75rem]">
-        
-        
-        
-      </div> */}
-    </div>
 
   )
 }
