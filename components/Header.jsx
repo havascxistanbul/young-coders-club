c5c6a69e809d2007741d2fb81937b1dc577dced9
Batuhan Goksu <batuhangoksu@gmail.com>
2022-12-13 13:13:48 +0300

    update styling & formatting

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import NavigationDesktop from './NavigationDesktop';
-import NavigationMobile from './NavigationMobile';
+import React from "react";
+import NavigationDesktop from "./NavigationDesktop";
+import NavigationMobile from "./NavigationMobile";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className='header flex justify-center z-30 w-screen fixed bg-white'>
-
-      <div className='header__logo absolute top-0 left-0'>
-        <img className='header__logo__self' src='/logo.jpg' alt='YCC Logo' />
+    <div className="header flex justify-center z-30 w-screen fixed bg-white">
+      <div className="header__logo absolute top-0 left-0">
+        <Image src="/logo.jpg" alt="YCC Logo" width="130" height="80" />
       </div>
-
-      <div className='nav-bar lg:relative left-20 static'>
+      <div className="nav-bar lg:relative left-20 static flex items-center">
         <NavigationDesktop />
         <NavigationMobile />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
