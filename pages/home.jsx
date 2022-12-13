c9b9424a684ffa35d6ddab83eead6c517e9bd1f0
Batuhan Goksu <batuhangoksu@gmail.com>
2022-12-13 13:06:14 +0300

    cta button update & code formatting

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,91 +1,107 @@
 //Default
-import React, {useState, useEffect} from 'react'
-import Head from 'next/head';
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
 
 // Components
-import Header from '../components/Header';
-import HeroBanner from '../components/HeroBanner';
-import Inpage from '../components/Inpage';
-import Faq from '../components/Faq';
-import InpageWithBg from '../components/InpageWithBg';
-import TimingAndProcss from '../components/TimingAndProcess'
+import Header from "../components/Header";
+import HeroBanner from "../components/HeroBanner";
+import Inpage from "../components/Inpage";
+import Faq from "../components/Faq";
+import InpageWithBg from "../components/InpageWithBg";
+import TimingAndProcss from "../components/TimingAndProcess";
 
 // Contentful
-import { createClient } from 'contentful'
-
+import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.CF_SPACE_ID || process.env.NEXT_PUBLIC_CF_SPACE_ID,
-  accessToken: process.env.CF_ACCESS_TOKEN || process.env.NEXT_PUBLIC_CF_ACCESS_TOKEN,
-})
+  accessToken:
+    process.env.CF_ACCESS_TOKEN || process.env.NEXT_PUBLIC_CF_ACCESS_TOKEN,
+});
 
 export async function getServerSideProps(context) {
   const { items } = await client.getEntries({
-    content_type: 'page',
-  })
+    content_type: "page",
+  });
 
   const components = items[0].fields.components;
   const content = {
     components,
-  }
+  };
 
   // Get FAQ Accordion Items
-  const resFaq = await client.getEntries({ content_type: 'faq' });
+  const resFaq = await client.getEntries({ content_type: "faq" });
   const faq = resFaq.items;
 
   // Get Timing & Process Accordion Items
-  const resTimingAndProcess = await client.getEntries({ content_type: 'courseContent' });
+  const resTimingAndProcess = await client.getEntries({
+    content_type: "courseContent",
+  });
   const timingAndProcess = resTimingAndProcess.items;
 
   // const { slug } = context.params;
   return {
     props: { content, faq, timingAndProcess },
-  }
+  };
 }
 
 export default function Page(props) {
   const [heroBannerItems, setHeroBannerItems] = useState({
     image: {
-      fileName: '',
-      url: '',
+      fileName: "",
+      url: "",
     },
     cta: {
-      text: '',
-      url: '#'
+      text: "",
+      url: "https://www.linkedin.com/jobs/view/3393155732/?refId=i17dQbK0YAVu0VbuA4ktuQ%3D%3D",
     },
-    id: '',
+    id: "",
   });
 
   const [inpageContent, setInpageContent] = useState([]);
 
   useEffect(() => {
     const components = props.content.components;
-    const heroBanner = components.filter((item) => item.sys.contentType.sys.id === 'heroBanner')[0];
+    const heroBanner = components.filter(
+      (item) => item.sys.contentType.sys.id === "heroBanner"
+    )[0];
 
-    setInpageContent(components.filter((item) => item.sys.contentType.sys.id === 'inpageContent'));
+    setInpageContent(
+      components.filter(
+        (item) => item.sys.contentType.sys.id === "inpageContent"
+      )
+    );
 
     const heroBannerCtaID = heroBanner.fields.cta.sys.id;
     const heroBannerImage = heroBanner.fields.image.fields.file;
     const componentId = heroBanner.fields.componentId;
 
-    client.getEntry(heroBannerCtaID)
+    client
+      .getEntry(heroBannerCtaID)
       .then((entry) => {
         setHeroBannerItems({
           image: heroBannerImage,
           cta: entry.fields,
-          id: componentId
-        })
+          id: componentId,
+        });
       })
-      .catch(console.error)
-  }, [props, setHeroBannerItems, setInpageContent])
+      .catch(console.error);
+  }, [props, setHeroBannerItems, setInpageContent]);
 
   return (
     <>
       <Head>
         <title>Young Coders Club</title>
-        <meta property="og:title" content='Young Coders Club' key="title" />
-        <meta property="description" content="Want to work in our global IT projets? Join Young Coders Club for our free training session and kickstart your career!" />
-        <meta property="og:description" content="Want to work in our global IT projets? Join Young Coders Club for our free training session and kickstart your career!" key="description" />
+        <meta property="og:title" content="Young Coders Club" key="title" />
+        <meta
+          property="description"
+          content="Want to work in our global IT projets? Join Young Coders Club for our free training session and kickstart your career!"
+        />
+        <meta
+          property="og:description"
+          content="Want to work in our global IT projets? Join Young Coders Club for our free training session and kickstart your career!"
+          key="description"
+        />
         <link rel="shortcut icon" href="/favicon.png" />
         <meta property="og:image" key="og_image" content="/favicon.png" />
       </Head>
@@ -94,13 +110,18 @@ export default function Page(props) {
         <HeroBanner {...heroBannerItems} />
         {inpageContent.map((item) => (
           <div key={item.fields.name}>
-            {item.fields.componentId === 'supporters'? <InpageWithBg {...item.fields} /> : <Inpage {...item.fields} />}
-            {item.fields.componentId === 'faq' && <Faq {...props.faq} />}
-            {item.fields.componentId === 'timing-and-process' && <TimingAndProcss {...props.faq} />}
+            {item.fields.componentId === "supporters" ? (
+              <InpageWithBg {...item.fields} />
+            ) : (
+              <Inpage {...item.fields} />
+            )}
+            {item.fields.componentId === "faq" && <Faq {...props.faq} />}
+            {item.fields.componentId === "timing-and-process" && (
+              <TimingAndProcss {...props.faq} />
+            )}
           </div>
         ))}
       </main>
     </>
-  )
+  );
 }
-
