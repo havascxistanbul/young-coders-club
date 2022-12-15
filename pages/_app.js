618ecd1b99969f93af3a9b353f7f30c8ee05f017
Baran Kandil <barankandil10@gmail.com>
2022-12-15 18:22:18 +0300

    Add GA Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import '../styles/style.css'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-XNJGE10D8C`}
+        />
+        <Script id="GA-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XNJGE10D8C', {
+              page_path: window.location.pathname,
+            });
+        `}
+        </Script>
+        <Component {...pageProps} />
+      </>
+    )
 }
 
 export default MyApp
